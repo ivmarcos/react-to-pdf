0fcfe1f4397c71be0a3b6c9c56ff1d54d10081b7
Marcos Andrei Ivanechtchuk <ivmarcos@gmail.com>
2023-09-07 03:53:43 -0300

    undefined

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -48,7 +48,7 @@ describe("template spec", () => {
     examples.forEach((example) => {
       cy.task("compareFile", example.filename).then(
         (result: ComparisonResult) => {
-          expect(result.message).to.be.empty;
+          expect(result.message).to.be.undefined;
           expect(result.status).to.equal("passed");
         }
       );
