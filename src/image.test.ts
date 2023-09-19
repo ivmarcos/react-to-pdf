05f055887adc2ed5a5928369846564bfb7aa6f6c
Marcos Andrei Ivanechtchuk <ivmarcos@gmail.com>
2023-09-19 02:26:30 -0300

    tests for image

---
+++ src/image.test.ts
@@ -0,0 +1,23 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { test, expect } from "@jest/globals";
+import * as utils from './utils'
+import { Image } from "./image";
+
+describe('Image', () => {
+
+  test('should return the correct dimensions', () => {
+    const canvas = document.createElement('canvas');
+    canvas.setAttribute('width', String(200))
+    canvas.setAttribute('height', String(400));
+    const image = new Image(canvas, 2);
+    expect({width: image.getWidth(), height: image.getHeight(), originalWidth: image.getOriginalWidth(), originalHeight: image.getOriginalHeight()}).toEqual({
+      width: 200,
+      height: 400,
+      originalWidth: 100,
+      originalHeight: 200
+    })
+  })
+})
