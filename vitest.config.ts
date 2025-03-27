f9d7dd6364177a83368fe1e0400d0fcd76f46075
Marcos Andrei Ivanechtchuk <ivmarcos@gmail.com>
2025-03-27 19:33:42 -0300

    configure vitest and esbuild build

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "react-to-pdf": process.env.CI ? '../dist' : "../src",
+      "react-to-pdf": "../src",
     },
+  },
+  test: {
+    globals: true,
   },
 });
