d3e17701f00b06db82f4266f8798b240f3bfedeb
Marcos Andrei Ivanechtchuk <ivmarcos@gmail.com>
2025-03-28 12:24:27 -0300

    format files

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ export default {
   input: "src/index.ts",
   output: {
     file: "dist/index.d.ts",
-    format: "es"
+    format: "es",
   },
-  plugins: [dts()]
+  plugins: [dts()],
 };
--- scripts/build.js
+++ scripts/build.js
@@ -1,31 +1,30 @@
 import esbuild from "esbuild";
 
-const buildDir = 'dist';
+const buildDir = "dist";
 const entryFile = "src/index.ts";
 
 const configs = [
   {
-    format: 'esm',
-    outFile: 'index.mjs'
+    format: "esm",
+    outFile: "index.mjs",
   },
   {
-    format: 'cjs',
-    outFile: 'index.js'
-  }
-]
+    format: "cjs",
+    outFile: "index.js",
+  },
+];
 
-configs.forEach(config => {
+configs.forEach((config) => {
   esbuild
-  .build({
-    entryPoints: [entryFile],
-    outfile: `${buildDir}/${config.outFile}`,
-    bundle: true,
-    external: ["react", "react-dom", "jspdf", "html2canvas"],
-    target: "esnext",
-    format: config.format,
-    platform: "neutral",
-    sourcemap: true,
-  })
-  .catch(() => process.exit(1));
-
-})
+    .build({
+      entryPoints: [entryFile],
+      outfile: `${buildDir}/${config.outFile}`,
+      bundle: true,
+      external: ["react", "react-dom", "jspdf", "html2canvas"],
+      target: "esnext",
+      format: config.format,
+      platform: "neutral",
+      sourcemap: true,
+    })
+    .catch(() => process.exit(1));
+});
