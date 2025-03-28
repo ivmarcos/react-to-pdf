4394d005a80ca43c0d36cb55c5b128104e240f5c
Marcos Andrei Ivanechtchuk <ivmarcos@gmail.com>
2025-03-28 08:52:36 -0300

    fix prettier

--- scripts/build.js
+++ scripts/build.js
@@ -1,21 +1,25 @@
-import esbuild from 'esbuild';
-import { execSync } from 'child_process';
+import esbuild from "esbuild";
+import { execSync } from "child_process";
 
-const entryFile = 'src/index.ts';
-const outFile = 'dist/index.mjs';
-const dtsFile = 'dist/types.d.ts';
+const entryFile = "src/index.ts";
+const outFile = "dist/index.mjs";
+const dtsFile = "dist/types.d.ts";
 
 // Run TypeScript compiler to generate declaration file
-execSync(`tsc ${entryFile} --declaration --emitDeclarationOnly --outFile ${dtsFile}`);
+execSync(
+  `tsc ${entryFile} --declaration --emitDeclarationOnly --outFile ${dtsFile}`
+);
 
 // Run esbuild to bundle the JavaScript output
-esbuild.build({
-  entryPoints: [entryFile],
-  outfile: outFile,
-  bundle: true,
-  external: ['react', 'react-dom', 'jspdf', 'html2canvas'],
-  target: 'esnext',  
-  format: "esm",
-  platform: 'neutral',
-  sourcemap: true,
-}).catch(() => process.exit(1));
+esbuild
+  .build({
+    entryPoints: [entryFile],
+    outfile: outFile,
+    bundle: true,
+    external: ["react", "react-dom", "jspdf", "html2canvas"],
+    target: "esnext",
+    format: "esm",
+    platform: "neutral",
+    sourcemap: true,
+  })
+  .catch(() => process.exit(1));
